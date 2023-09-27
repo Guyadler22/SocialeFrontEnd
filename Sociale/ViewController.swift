11db6dc30060f054ecec14c852fbd5996385b673
Guy Adler <Guy7500@gmail.com>
2023-09-27 18:04:50 +0300

    Initial Commit

---
+++ Sociale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sociale
+//
+//  Created by Guy Adler on 27/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
